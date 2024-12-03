0ad943c9ab83ee626e9b76c41d3f6a9b30ed20f0
InesMedem <inesmedem93@gmail.com>
2024-12-04 10:09:27 +1100

    plugin development

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react'), 'version' => '18a855b78fa0276a5d9d');
--- index.php
+++ index.php
@@ -1,22 +1,32 @@
 <?php
 
 /*
-  Plugin Name: Are You Paying Attention Quiz
+  Plugin Name: Are You Paying Attention Quiz 4 dec
   Description: Give your readers a multiple choice question.
   Version: 1.0
-  Author: Ines
-  Author URI: https://www.inesmedem.com
+  Author: Brad
+  Author URI: https://www.udemy.com/user/bradschiff/
 */
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 class AreYouPayingAttention {
   function __construct() {
-    add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+    add_action('init', array($this, 'adminAssets'));
   }
 
   function adminAssets() {
-    wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'test.js', array('wp-blocks', 'wp-element'));
+    wp_register_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element'));
+    register_block_type('ourplugin/are-you-paying-attention', array(
+      'editor_script' => 'ournewblocktype',
+      'render_callback' => array($this, 'theHTML')
+    ));
+  }
+
+  function theHTML($attributes) {
+    ob_start(); ?>
+    <h3>Today the sky is <?php echo esc_html($attributes['skyColor']) ?> and the grass is <?php echo esc_html($attributes['grassColor']) ?>!</h3>
+    <?php return ob_get_clean();
   }
 }
 
